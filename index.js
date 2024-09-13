cfe6e5217577511ddb882461ef8ca902d687ca27
rampalliprakhar <rampalliprakhar@gmail.com>
2024-09-12 23:46:30 -0400

    Completed Socket Setup

--- index.js
+++ index.js
@@ -11,6 +11,18 @@ const io = new Server(httpServer, {cors: "http://localhost:3000/"});
 
 io.on("connection", (socket) => {
     console.log("server connected")
-});
+  
+    socket.on('startPosition', (arg) => {
+      socket.broadcast.emit('startPosition', arg)
+    })
+  
+    socket.on('draw', (arg) => {
+      socket.broadcast.emit('draw', arg)
+    })
+  
+    socket.on('changeConfig', (arg) => {
+      socket.broadcast.emit('changeConfig', arg)
+    })
+  });
 
 httpServer.listen(5000);
