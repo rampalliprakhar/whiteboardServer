14e6c8bbed156ef90b55c6767cf8506d62b87023
Prakhar <rampalliprakhar@gmail.com>
2025-03-26 22:41:18 -0400

    Updated CORS

--- index.js
+++ index.js
@@ -4,11 +4,17 @@ const { Server } = require("socket.io");
 const cors = require("cors");
 
 const app = express();
-const isDev = app.settings.env === 'development'
-const URL = isDev ? 'http://localhost:3000' : process.env.WEBSITE_URL;
-app.use(cors({origin: URL}))
+const isDev = process.env.NODE_ENV === 'development'
+const allowedOrigin = isDev 
+  ? 'http://localhost:3000' 
+  : process.env.WEBSITE_URL;
+
+app.use(cors({
+  origin: allowedOrigin
+}))
+
 const httpServer = createServer(app);
-const io = new Server(httpServer, { cors: URL });
+const io = new Server(httpServer, { cors: { origin: allowedOrigin } });
 
 let drawUsers = new Set();
 
